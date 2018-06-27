c3e7ff6af39c4f736bce49581a8db0132e924851
Chris Hamer-Hodges <chris.hamer-hodges@autodesk.com>
2018-06-27 15:20:53 +0100

    Solve using filter and all

--- koans/AboutApplyingWhatWeHaveLearnt.js
+++ koans/AboutApplyingWhatWeHaveLearnt.js
@@ -32,7 +32,7 @@ describe("About Applying What We Have Learnt", function() {
         }
     }
 
-    expect(productsICanEat.length).toBe(FILL_ME_IN);
+    expect(productsICanEat.length).toBe(1);
   });
 
   it("given I'm allergic to nuts and hate mushrooms, it should find a pizza I can eat (functional)", function () {
@@ -40,8 +40,10 @@ describe("About Applying What We Have Learnt", function() {
       var productsICanEat = [];
 
       /* solve using filter() & all() / any() */
+      var notMushrooms = function(x) { return x != "mushrooms"};
+      productsICanEat = _(products).filter(function(x){ return !x.containsNuts && _(x.ingredients).all(notMushrooms); });
 
-      expect(productsICanEat.length).toBe(FILL_ME_IN);
+      expect(productsICanEat.length).toBe(1);
   });
 
   /*********************************************************************************/
